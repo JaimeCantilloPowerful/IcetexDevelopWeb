5f19b23e6937714f0786a2e646e75904b803a8aa
Jaime Cantillo <cantillojaime10k@gmail.com>
2023-10-20 00:14:36 -0500

    Sesion 27/09 Part 2

--- main.js
+++ main.js
@@ -30,3 +30,48 @@ botonConsignar.style.height = "200px";
 titulo.appendChild(botonConsignar);
 
 botonConsignar.className = "arcoiris";
+
+botonConsignar.addEventListener("click", ()=>{
+    console.log("Haz hecho click");
+})
+
+let saldo = 500;
+
+let boton1 = document.getElementById("add"); //Aumentar
+let boton2 = document.getElementById("minus"); //Disminuir
+let saldoText = document.getElementById("Saldo");
+let transaciones = document.getElementById("transacciones");
+
+function actualizarSaldo(){
+    saldoText.innerHTML = `saldo : $${saldo}` 
+};
+
+boton1.addEventListener("click", ()=>{
+    saldo += 10;
+    let p = document.createElement("p");
+    p.innerText = "Se ha agregado $10 al saldo.";
+    transaciones.appendChild(p);
+    actualizarSaldo(); 
+});
+
+boton2.addEventListener("click", ()=>{
+    saldo -= 10;
+    let p = document.createElement("p");
+    p.innerText = "Se ha quitado $10 del saldo.";
+    transaciones.appendChild(p);
+    actualizarSaldo();
+});
+
+//Nueva funcionalidad
+let realizarConsignacion = document.getElementById("consignar");
+
+realizarConsignacion.addEventListener("click", ()=>{
+    event.preventDefault();
+    let cuentaConsignacion = document.getElementById("cuentaConsignar");
+    saldo -= 10;
+    let p = document.createElement("p");
+    p.innerHTML =`Consignaste $10 a la cuenta ${cuentaConsignacion.value}`;
+
+    transaciones.appendChild(p);
+    actualizarSaldo ();
+});
